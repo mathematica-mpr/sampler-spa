3f6b1688a813173ab8b85f03ba0407423baaa166
Maxime Dulieu <MDulieu@mathematica-mpr.com>
2019-05-09 13:28:17 -0400

    chaptermap

--- src/app/core/chapter.service.ts
+++ src/app/core/chapter.service.ts
@@ -1,14 +1,28 @@
-import { Injectable } from '@angular/core';
-import { Chapter } from './models/chapter';
+import { Injectable, Type } from '@angular/core';
+import { Chapter, ChapterElement } from './models/chapter';
 import { mockChapter } from 'test/mockChapter';
+import { NumberInputComponent } from '../shared/input/number-input/number-input.component';
+import { ChapterItem } from './models/chapter-item';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ChapterService {
+    chapterItemMap = {
+        number: NumberInputComponent
+    };
+
     constructor() {}
 
     getChapter(curChapter: number): Chapter {
         return mockChapter;
+    }
+
+    mapChapterItem(chapterElementType: string): Type<any> {
+        return this.chapterItemMap[chapterElementType];
+    }
+
+    getChapterItem(chapterElement: ChapterElement): ChapterItem {
+        return new ChapterItem(this.mapChapterItem(chapterElement.type), chapterElement);
     }
 }
