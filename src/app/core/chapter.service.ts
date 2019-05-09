d02ae716a5b93af296e812317f19b9b63d8d38df
Maxime Dulieu <MDulieu@mathematica-mpr.com>
2019-05-09 15:12:51 -0400

    typeName + factory trial

--- src/app/core/chapter.service.ts
+++ src/app/core/chapter.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, Type } from '@angular/core';
+import {
+    Injectable,
+    Type,
+    ComponentFactoryResolver,
+    ViewContainerRef
+} from '@angular/core';
 import { Chapter, ChapterElement } from './models/chapter';
 import { mockChapter } from 'test/mockChapter';
 import { NumberInputComponent } from '../shared/input/number-input/number-input.component';
@@ -11,16 +16,26 @@ import { LineGraphComponent } from '../shared/graph/line-graph/line-graph.compon
 })
 export class ChapterService {
     chapterItemMap = {
-        section: SectionDescriptionComponent,
-        number: NumberInputComponent,
-        line: LineGraphComponent
+        SectionDescription: SectionDescriptionComponent,
+        NumberInput: NumberInputComponent,
+        LineGraph: LineGraphComponent
     };
 
-    constructor() {}
+    constructor(private resolver: ComponentFactoryResolver) {}
 
     getChapter(curChapter: number): Chapter {
         return mockChapter;
     }
+
+    // ChapterItemFactory(chapterElementType: string): Type<any> {
+    //     //  https://stackoverflow.com/questions/40115072/how-to-load-component-dynamically-using-component-name-in-angular2
+    //     const factories = Array.from(this.resolver['_factories'].keys());
+    //     const factoryClass = <Type<any>>(
+    //         factories.find((x: any) => x.name === chapterElementType)
+    //     );
+    //     const factory = this.resolver.resolveComponentFactory(factoryClass);
+    //     return factory.componentType;
+    // }
 
     mapChapterItem(chapterElementType: string): Type<any> {
         return this.chapterItemMap[chapterElementType];
