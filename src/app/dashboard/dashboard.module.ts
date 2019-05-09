fc08d20956b7bc02e71e9525e9b2b711d124ebc6
Maxime Dulieu <MDulieu@mathematica-mpr.com>
2019-05-09 13:28:41 -0400

    entryCompotnent

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardViewComponent } from './dashboard-view/dashboard-view.component';
-import { ChapterDescriptionComponent } from '../shared/chapter/chapter-description/chapter-description.component';
-import { ChapterInputsComponent } from '../shared/chapter/chapter-inputs/chapter-inputs.component';
-import { ChapterGraphsComponent } from '../shared/chapter/chapter-graphs/chapter-graphs.component';
+import { ChapterItemComponent } from '../shared/chapter/chapter-item/chapter-item.component';
+import { NumberInputComponent } from '../shared/input/number-input/number-input.component';
+import { ChapterDirective } from '../shared/chapter/chapter.directive';
 
 @NgModule({
     imports: [CommonModule, DashboardRoutingModule],
     declarations: [
         DashboardViewComponent,
-        ChapterDescriptionComponent,
-        ChapterInputsComponent,
-        ChapterGraphsComponent
-    ]
+        ChapterItemComponent,
+        NumberInputComponent,
+        ChapterDirective
+    ],
+    entryComponents: [NumberInputComponent]
 })
 export class DashboardModule {}
