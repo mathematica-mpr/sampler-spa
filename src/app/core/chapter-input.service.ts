eac626823f9fc69831ad179d90a4b8777da4c8b3
Maxime Dulieu <MDulieu@mathematica-mpr.com>
2019-05-16 11:07:04 -0400

    inputformGroup

--- src/app/core/chapter-input.service.ts
+++ src/app/core/chapter-input.service.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/internal/Subject';
+import { ChapterInput } from './models/chapter';
+import { FormGroup, FormControl } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ChapterInputService {
-    private chapterInputSource = new Subject<number>();
-    chapterInput$ = this.chapterInputSource.asObservable();
+    inputFormGroup: FormGroup;
+
     constructor() {}
 
-    inputChapter(input: number) {
-        this.chapterInputSource.next(input);
+    setInputFormGroup(chapterInputs: ChapterInput[]) {
+        this.inputFormGroup = this.getInputFormGroup(chapterInputs);
+    }
+
+    getInputFormGroup(chapterInputs: ChapterInput[]): FormGroup {
+        let formGroup = new FormGroup({});
+        chapterInputs.forEach((chapterInput: ChapterInput) => {
+            formGroup.addControl(chapterInput.name, this.getFormControl(chapterInput));
+        });
+        return formGroup;
+    }
+
+    getFormControl(chapterInput: ChapterInput): FormControl {
+        // TODO: pass initial values
+        return new FormControl();
     }
 }
