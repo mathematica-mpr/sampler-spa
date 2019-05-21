9638ad8b87137519c7dad30cb056e2d9f782f778
Maxime Dulieu <MDulieu@mathematica-mpr.com>
2019-05-21 10:48:11 -0400

    update property

--- src/app/core/chapter.service.ts
+++ src/app/core/chapter.service.ts
@@ -3,15 +3,23 @@ import { Chapter, ChapterElement } from './models/chapter';
 import { mockChapter } from 'test/mockChapter';
 import { ChapterItem } from './models/chapter-item';
 import { ChapterItemMap } from './chapter-item.resource';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ChapterService {
+    chapter: BehaviorSubject<Chapter> = new BehaviorSubject(null);
+
     constructor() {}
 
-    getChapter(curChapter: number): Chapter {
-        return mockChapter;
+    updateChapterProperties(newProps: Partial<Chapter>) {
+        let updateChapter = { ...this.chapter.value, ...newProps };
+        this.chapter.next(updateChapter);
+    }
+
+    setChapter(curChapter: number): void {
+        this.chapter.next(mockChapter);
     }
 
     getComponentType(chapterElementType: string): Type<any> {
