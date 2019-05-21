feda230acb8dcba428724504ae80b1b5c6665bf6
Maxime Dulieu <MDulieu@mathematica-mpr.com>
2019-05-20 20:45:28 -0400

    chapterElement

--- src/app/core/models/chapter-item.ts
+++ src/app/core/models/chapter-item.ts
@@ -1,5 +1,6 @@
 import { Type } from '@angular/core';
+import { ChapterElement } from './chapter';
 
 export class ChapterItem {
-    constructor(public component: Type<any>, public data: any) {}
+    constructor(public component: Type<any>, public chapterElement: ChapterElement) {}
 }
