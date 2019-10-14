b60b3920610c53254dd3256f076674ff9ed4e8cc
Maxime Dulieu <MDulieu@mathematica-mpr.com>
2019-10-14 16:44:49 -0400

    zoom service

--- src/app/shared/graph/confusion-error-graph/confusion-error-graph.component.ts
+++ src/app/shared/graph/confusion-error-graph/confusion-error-graph.component.ts
@@ -3,6 +3,7 @@ import { CompositeGraph } from '../composite-graph';
 import { BehaviorSubject } from 'rxjs';
 import { ChapterElement } from '../../../core/models/chapter';
 import * as d3 from 'd3';
+import { ZoomService } from 'src/app/core/zoom.service';
 
 @Component({
     selector: 'app-confusion-error-graph',
@@ -15,7 +16,7 @@ export class ConfusionErrorGraphComponent extends CompositeGraph implements OnIn
     instantiated = false;
     hoveredId = null;
     zoomedId = null;
-    constructor(private elRef: ElementRef) {
+    constructor(private elRef: ElementRef, private zoomService: ZoomService) {
         super();
     }
 
@@ -39,20 +40,11 @@ export class ConfusionErrorGraphComponent extends CompositeGraph implements OnIn
         const currentDiv = this.elRef.nativeElement.querySelector('.graph.' + id);
 
         if (this.zoomedId === id) {
-            currentDiv.classList.remove('zoomed');
             this.zoomedId = null;
+            this.zoomService.zoomOut(currentDiv);
         } else {
-            const width = window.innerWidth;
-            const height = window.innerHeight;
-            const bbox = currentDiv.getBoundingClientRect();
-            const xDenom: number = width < height ? 4 : 3;
-            const yDenom: number = width < height ? 3 : 4;
-            const tx = (width - bbox.width * 2) / xDenom - bbox.x;
-            const ty = (height - bbox.height * 2) / yDenom - bbox.y;
             this.zoomedId = id;
-            currentDiv.classList.add('zoomed');
-            currentDiv.style.setProperty('--x', tx + 'px');
-            currentDiv.style.setProperty('--y', ty + 'px');
+            this.zoomService.zoomIn(currentDiv);
         }
     }
 
