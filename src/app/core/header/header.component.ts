de08f59ac369d46452edca9aceaeef7428d2ccbe
Maxime Dulieu <MDulieu@mathematica-mpr.com>
2020-02-07 18:45:34 -0500

    sidenave open close

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -5,8 +5,10 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-    constructor() {}
-
-    ngOnInit() {}
+export class HeaderComponent {
+    openNav() {
+        document.getElementById('mySidenav').style.width = '250px';
+        document.getElementById('mySidenav').classList.add('extended');
+        document.getElementById('main').style.marginLeft = '250px';
+    }
 }
