e379c818b1e54dad80a29f35beff6a7b5c77fb4b
Maxime Dulieu <MDulieu@mathematica-mpr.com>
2020-12-11 13:25:14 -0500

    thinds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { CoreModule } from './core/core.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, CoreModule],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        CoreModule,
+        NgbModule
+    ],
     providers: [],
     bootstrap: [AppComponent]
 })
